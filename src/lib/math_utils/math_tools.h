76b6e8ac474ca95db928154af2f3200abbf39244
Stephen Rowe <srowe12@gmail.com>
2015-09-07 15:58:42 -0600

    Add meshgrid function and unit test.
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- src/lib/math_utils/math_tools.h
+++ src/lib/math_utils/math_tools.h
@@ -18,5 +18,26 @@ std::vector<T> linspace(T a, T b, unsigned int num_points){
    return points;
 }
 
+template <typename T>
+std::array<std::vector<T>,2> meshgrid(std::vector<T> xpoints, std::vector<T> ypoints){
+
+   size_t num_points_x = xpoints.size();
+   size_t num_points_y = ypoints.size();
+   size_t num_points = num_points_x*num_points_y;
+   std::vector<T> X(num_points);
+   std::vector<T> Y(num_points);
+   T xval;
+   T yval;
+   for (size_t row = 0; row < num_points_x; row++){
+       xval = xpoints[row];
+        for (size_t col = 0; col < num_points_y; col++) {
+           yval = ypoints[col];
+           X[row*num_points_y + col] = xval;
+           Y[row*num_points_y+col] = yval;
+       }
+   }
+   std::array<std::vector<T>,2> return_points{X,Y};
+  return return_points;
+}
 } //namespace mathtools
 #endif //MATH_UTILS_HDR
--- src/lib/math_utils/test/math_test.cpp
+++ src/lib/math_utils/test/math_test.cpp
@@ -2,7 +2,7 @@
 #include "../math_tools.h"
 #include <stdio.h>
 
-TEST(MathTest,FakeTest){
+TEST(MathTest,LinspaceTest){
    double a = 0;
    double b = 1;
    unsigned int num_points = 50;
@@ -18,6 +18,35 @@ TEST(MathTest,FakeTest){
    EXPECT_EQ(num_points+1,points_odd.size());
    EXPECT_DOUBLE_EQ(a, points_odd[0]);
    EXPECT_DOUBLE_EQ(b, points_odd[num_points]);
+}
+
+TEST(MathTest, MeshgridTest){
+   double ax = 3;
+   double bx = 4;
+   size_t num_x = 4;
+   std::vector<double> xpoints = mathtools::linspace<double>(ax,bx,num_x);
+
+   double ay = 1;
+   double by = 2;
+   size_t num_y = 2;
+   std::vector<double> ypoints = mathtools::linspace<double>(ay,by,num_y);
+
+   std::array<std::vector<double>,2> pointset = mathtools::meshgrid<double>(xpoints,ypoints);
+   std::vector<double> xvals = pointset[0];
+   std::vector<double> yvals = pointset[1];
+ 
+   size_t num_vals = (num_x+1)*(num_y+1);
+   EXPECT_EQ(num_vals,xvals.size() );
+   EXPECT_EQ(num_vals, yvals.size() );
+   
+   EXPECT_DOUBLE_EQ(xvals[0], ax);
+   EXPECT_DOUBLE_EQ(xvals[num_vals-1],bx);
+   EXPECT_DOUBLE_EQ(xvals[3], 3.25);
+   EXPECT_DOUBLE_EQ(xvals[num_vals-4],3.75);
+   EXPECT_DOUBLE_EQ(yvals[0], ay);
+   EXPECT_DOUBLE_EQ(yvals[num_vals-1],by);
+   EXPECT_DOUBLE_EQ(yvals[3], 1);
+   EXPECT_DOUBLE_EQ(yvals[num_vals-4],2);
 }
 
 int main(int argc, char** argv) {
