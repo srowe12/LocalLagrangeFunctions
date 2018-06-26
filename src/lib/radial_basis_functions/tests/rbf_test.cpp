2aecc8ee2ab8e1c9bf85f2a4633bf89278c011cc
Stephen Rowe <srowe12@gmail.com>
2018-06-26 12:00:07 -0600

    Add first failing test
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- src/lib/radial_basis_functions/tests/rbf_test.cpp
+++ src/lib/radial_basis_functions/tests/rbf_test.cpp
@@ -10,9 +10,10 @@ TEST(RbfTest, ConstructoGaussianInterpolant) {
   // We build up a Gaussian interpolant for 2D data
   const double a = 0.0;
   const double b = 1.0;
+  const int n = 20;
   auto x_mesh = mathtools::linspace(a, b, 20);
   const arma::mat data = mathtools::meshgrid(x_mesh, x_mesh);
-
+  const size_t N = data.n_rows;
   // Use this data to build up a Gaussian interpolant
 
   Gaussian<double> gaussian(1.0);
@@ -22,8 +23,20 @@ TEST(RbfTest, ConstructoGaussianInterpolant) {
     sampled_data(i) = std::sin(data(i, 0)) * std::cos(data(i, 1));
   }
 
-  RadialBasisFunctionInterpolant<Gaussian<double>, 1>(gaussian, data,
+  RadialBasisFunctionInterpolant<Gaussian<double>, 1> interpolant(gaussian, data,
                                                       sampled_data);
+
+  // Compute on the same dataset and see how well we do! We should get zeros back
+  const auto interpolated_data = interpolant.interpolate(data);
+
+  ASSERT_EQ(N, interpolated_data.n_rows);
+
+  for (size_t i = 0; i < N ; ++i) {
+  	EXPECT_NEAR(0.0, interpolated_data(i), 1e-9);
+  }
+
+
+
 }
 
 int main(int argc, char **argv) {
