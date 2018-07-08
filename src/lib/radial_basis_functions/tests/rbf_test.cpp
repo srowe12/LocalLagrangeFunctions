8b0b32e89d5933def33cec13b1e11d69f401f70b
Stephen Rowe <srowe12@gmail.com>
2018-07-08 12:57:04 -0600

    Add a bigger Gaussian interp test and a constant interp test
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- src/lib/radial_basis_functions/tests/rbf_test.cpp
+++ src/lib/radial_basis_functions/tests/rbf_test.cpp
@@ -27,8 +27,39 @@ TEST(RbfTest, ConstructoGaussianInterpolant) {
   // Use this data to build up a Gaussian interpolant
 
   Gaussian<double> gaussian(4.0);
-  std::cout << "The N is " << N << std::endl;
-  data.print("Data");
+
+  arma::vec sampled_data(N);
+  for (size_t i = 0; i < N; ++i) {
+    sampled_data(i) = std::sin(data(i, 0)) * std::cos(data(i, 1));
+  }
+
+  RadialBasisFunctionInterpolant<Gaussian<double>, 1> interpolant(
+      gaussian, data, sampled_data);
+
+  // Compute on the same dataset and see how well we do! We should get zeros
+  // back
+
+  const arma::vec interpolated_data = interpolant.interpolate(data);
+
+  ASSERT_EQ(N, interpolated_data.n_rows);
+
+  for (size_t i = 0; i < N; ++i) {
+    EXPECT_NEAR(sampled_data(i), interpolated_data(i), 1e-7);
+  }
+}
+
+TEST(RbfTest, ConstructoGaussianInterpolantBigger) {
+  // We build up a Gaussian interpolant for 2D data
+  const double a = 0.0;
+  const double b = 1.0;
+  const int n = 20;
+  auto x_mesh = mathtools::linspace(a, b, n);
+  const arma::mat data = mathtools::meshgrid(x_mesh, x_mesh);
+  const size_t N = data.n_rows;
+  // Use this data to build up a Gaussian interpolant
+
+  Gaussian<double> gaussian(4.0);
+
   arma::vec sampled_data(N);
   for (size_t i = 0; i < N; ++i) {
     sampled_data(i) = std::sin(data(i, 0)) * std::cos(data(i, 1));
@@ -46,8 +77,38 @@ TEST(RbfTest, ConstructoGaussianInterpolant) {
   for (size_t i = 0; i < N ; ++i) {
     EXPECT_NEAR(sampled_data(i), interpolated_data(i), 1e-7);
   }
+}
 
+TEST(RbfTest, ConstructoGaussianInterpolantConstant) {
+  // We build up a Gaussian interpolant for 2D data
+  const double a = 0.0;
+  const double b = 1.0;
+  const int n = 4;
+  auto x_mesh = mathtools::linspace(a, b, 4);
+  const arma::mat data = mathtools::meshgrid(x_mesh, x_mesh);
+  const size_t N = data.n_rows;
+  // Use this data to build up a Gaussian interpolant
 
+  Gaussian<double> gaussian(4.0);
+
+  arma::vec sampled_data(N);
+  for (size_t i = 0; i < N; ++i) {
+    sampled_data(i) = 1.0;
+  }
+
+  RadialBasisFunctionInterpolant<Gaussian<double>, 1> interpolant(
+      gaussian, data, sampled_data);
+
+  // Compute on the same dataset and see how well we do! We should get zeros
+  // back
+
+  const arma::vec interpolated_data = interpolant.interpolate(data);
+
+  ASSERT_EQ(N, interpolated_data.n_rows);
+
+  for (size_t i = 0; i < N; ++i) {
+    EXPECT_NEAR(sampled_data(i), interpolated_data(i), 1e-7);
+  }
 }
 
 int main(int argc, char **argv) {
