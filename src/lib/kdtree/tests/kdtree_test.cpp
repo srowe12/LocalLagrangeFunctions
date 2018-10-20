e915da7757da615bc52a86c6ec090b98d73001a0
Stephen Rowe <srowe12@gmail.com>
2018-10-20 13:43:53 -0600

    Add unit test for even number of points on insertion into tree
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- src/lib/kdtree/tests/kdtree_test.cpp
+++ src/lib/kdtree/tests/kdtree_test.cpp
@@ -7,15 +7,35 @@ TEST(KdtreeTests, SimpleTest) {
 
   arma::mat points{{0, 0}, {1, 1}, {0, 1}, {1, 0}, {.5, .5}};
 
-  std::cout << "Starting tree" << std::endl;
   auto tree = BuildTree<2>(points);
-  std::cout << "Done building" << std::endl;
+
   arma::rowvec point{0, 0};
   for (size_t i = 0; i < points.n_rows; ++i) {
     arma::rowvec point = points.row(i);
     bool in_tree = search<2>(tree, point);
-    point.print("The point is");
-    std::cout << "The success is" << in_tree << std::endl;
+
     EXPECT_TRUE(in_tree);
   }
+
+  arma::mat bad_point{{.3, .3}};
+  bool in_tree = search<2>(tree, bad_point);
+  EXPECT_FALSE(in_tree);
+}
+
+TEST(KdtreeTests, EvenNumberPointsBuildTree) {
+
+  arma::mat points{{0, 0}, {1, 1}, {0, 1}, {1, 0}};
+
+  auto tree = BuildTree<2>(points);
+  arma::rowvec point{0, 0};
+  for (size_t i = 0; i < points.n_rows; ++i) {
+    arma::rowvec point = points.row(i);
+    bool in_tree = search<2>(tree, point);
+
+    EXPECT_TRUE(in_tree);
+  }
+
+  arma::mat bad_point{{.3, .3}};
+  bool in_tree = search<2>(tree, bad_point);
+  EXPECT_FALSE(in_tree);
 }
