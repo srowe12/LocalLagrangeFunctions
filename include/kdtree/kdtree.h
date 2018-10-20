9586e42e8163467e042a481302d2322bbaeaa1be
Stephen Rowe <srowe12@gmail.com>
2018-10-20 13:44:16 -0600

    Clang-format kdtree.h
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- include/kdtree/kdtree.h
+++ include/kdtree/kdtree.h
@@ -55,20 +55,20 @@ std::shared_ptr<Node<N>> insert(std::shared_ptr<Node<N>> root,
 }
 
 template <size_t N>
-bool ComparePoints(const arma::rowvec& p1, const arma::rowvec& p2) {
-	///@todo srowe; Use other stuff already written
-	double diff  = 0.0;
-	for (size_t i = 0; i < N; ++i) {
-		diff += (p1[i]-p2[i])*(p1[i]-p2[i]);
-	}
-	return diff < 1e-13;
+bool ComparePoints(const arma::rowvec &p1, const arma::rowvec &p2) {
+  ///@todo srowe; Use other stuff already written
+  double diff = 0.0;
+  for (size_t i = 0; i < N; ++i) {
+    diff += (p1[i] - p2[i]) * (p1[i] - p2[i]);
+  }
+  return diff < 1e-13;
 }
 
 // Searches a point in the KD tree the parameter depth is used to determine
 // current axis
 template <size_t N>
-bool searchRec(std::shared_ptr<Node<N>> root,
-               const arma::rowvec &point, unsigned depth = 0) {
+bool searchRec(std::shared_ptr<Node<N>> root, const arma::rowvec &point,
+               unsigned depth = 0) {
   // Base cases
   if (root == nullptr) {
     return false;
@@ -88,12 +88,12 @@ bool searchRec(std::shared_ptr<Node<N>> root,
 }
 
 template <size_t N>
-bool search(std::shared_ptr<Node<N>> root,
-            const arma::rowvec &point) {
+bool search(std::shared_ptr<Node<N>> root, const arma::rowvec &point) {
   return searchRec<N>(root, point, 0);
 }
 
-inline arma::mat SortOnColumnIndex(const arma::mat &points, const unsigned dimension) {
+inline arma::mat SortOnColumnIndex(const arma::mat &points,
+                                   const unsigned dimension) {
   // Slice down the column
   std::cout << "Sorting" << std::endl;
   arma::uvec indices = arma::sort_index(points.col(dimension));
@@ -104,7 +104,8 @@ inline arma::mat SortOnColumnIndex(const arma::mat &points, const unsigned dimen
 }
 
 template <size_t N>
-std::shared_ptr<Node<N>> BuildTree(const arma::mat &points, const unsigned int depth = 0) {
+std::shared_ptr<Node<N>> BuildTree(const arma::mat &points,
+                                   const unsigned int depth = 0) {
   unsigned axis = depth % N;
   std::cout << "Building Tree with axis " << axis << std::endl;
   arma::mat sorted_points = SortOnColumnIndex(points, axis);
@@ -116,39 +117,41 @@ std::shared_ptr<Node<N>> BuildTree(const arma::mat &points, const unsigned int d
   const size_t sorted_rows = sorted_points.n_rows;
   // Make the left one
   std::shared_ptr<Node<N>> left = std::shared_ptr<Node<N>>(nullptr);
-sorted_points.print("The available points are");
+  sorted_points.print("The available points are");
   int left_length = median_point;
-  
-  if (left_length > 1) {
-	  arma::mat left_points = sorted_points.rows(0, median_point - 1);
-    left = BuildTree<N>(left_points, depth + 1);
-  }
-  else {
-	  // Leaf point
-	  std::cout << " left_length = " << left_length << " so adding row(0)" << std::endl;
-	  arma::rowvec left_point = sorted_points.row(0);
-	  left = std::make_shared<Node<N>>(left_point, nullptr, nullptr);
-  }
 
+  if (left_length > 1) {
+    arma::mat left_points = sorted_points.rows(0, median_point - 1);
+    left = BuildTree<N>(left_points, depth + 1);
+  } else {
+    // Leaf point
+    std::cout << " left_length = " << left_length << " so adding row(0)"
+              << std::endl;
+    arma::rowvec left_point = sorted_points.row(0);
+    left = std::make_shared<Node<N>>(left_point, nullptr, nullptr);
+  }
 
   int right_length = sorted_rows - median_point;
   std::shared_ptr<Node<N>> right = std::shared_ptr<Node<N>>(nullptr);
 
   if (right_length > 1) {
-	    arma::mat right_points =
-      sorted_points.rows(median_point + 1, sorted_rows - 1);
-	  std::cout << " Going down the right tree now!" << std::endl;
+    arma::mat right_points =
+        sorted_points.rows(median_point + 1, sorted_rows - 1);
+    std::cout << " Going down the right tree now!" << std::endl;
     right = BuildTree<N>(right_points, depth + 1);
   }
   /*
   else {
-	  // We have a final node
-	  arma::rowvec right_point = sorted_points.row(median_point+1);
-	  right = std::make_shared<Node<N>>(right_point, nullptr, nullptr);
+          // We have a final node
+          arma::rowvec right_point = sorted_points.row(median_point+1);
+          right = std::make_shared<Node<N>>(right_point, nullptr, nullptr);
   }
   */
-std::cout << "The left and right lengths are " << left_length << " , " << right_length;
-std::cout << "With median point " << median_point << " and size of sorted_points " << sorted_points.n_rows << std::endl;
+  std::cout << "The left and right lengths are " << left_length << " , "
+            << right_length;
+  std::cout << "With median point " << median_point
+            << " and size of sorted_points " << sorted_points.n_rows
+            << std::endl;
   median_row.print("Adding this point!");
   return std::make_shared<Node<N>>(median_row, left, right);
 }
