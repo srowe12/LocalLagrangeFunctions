231ba92efbbd10b752ccbf58e6fbcc12795eeebc
Stephen Rowe <srowe12@gmail.com>
2019-01-27 12:10:18 -0700

    Add some statistics to speed test
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- src/lib/local_lagrange/test/speed_test.cpp
+++ src/lib/local_lagrange/test/speed_test.cpp
@@ -26,15 +26,29 @@ int main() {
   std::vector<double> xmesh = mathtools::linspace<double>(0, 1, num_points);
   auto centers = mathtools::meshgrid<double>(xmesh, xmesh);
 
-  size_t num_centers = centers.n_rows;
+  size_t numcenters = centers.n_rows;
 
-  auto llfs = local_lagrange::buildLocalLagrangeFunctions<2>(centers, 1e-1);
+  auto llfs = local_lagrange::buildLocalLagrangeFunctions<2>(centers, 3e-2);
 
   std::cout << "Run complete!" << std::endl;
   auto end = std::chrono::steady_clock::now();
   auto diff = end - start;
   std::cout << std::chrono::duration<double, std::milli>(diff).count()
             << "milliseconds" << std::endl;
-
+  auto llfset = llfs.localLagrangeFunctions();
+  std::vector<size_t> num_centers;
+  size_t min = 999999;
+  size_t max = 0;
+  size_t mean = 0;
+  for (const auto &llf : llfset) {
+    int num = llf.centers().n_rows;
+    num_centers.push_back(num);
+    mean += num;
+  }
+  mean /= num_centers.size();
+  auto it = std::max_element(num_centers.begin(), num_centers.end());
+  auto it2 = std::min_element(num_centers.begin(), num_centers.end());
+  std::cout << "The num centers is " << *it << " and the min is " << *it2
+            << " and the average num is " << mean << "\n";
   return 0;
 }
