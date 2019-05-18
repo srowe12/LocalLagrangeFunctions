52c4f8c6035fc2ef9812337291957162468d80a0
Stephen Rowe <srowe12@gmail.com>
2019-05-18 15:28:21 -0600

    Add functioning unit test for polynomial basis building and clean up
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- src/lib/math_utils/test/polynomial_test.cpp
+++ src/lib/math_utils/test/polynomial_test.cpp
@@ -13,7 +13,41 @@ template <size_t N> void print(const Tuples<N> &t) {
   }
 }
 TEST_CASE("BuildTuples") {
-  auto results = buildTuples<3, 4>();
-  print(results);
-  REQUIRE(1 == 0);
+  const auto results = buildTuples<3, 4>();
+
+  Tuples<3> expected;
+  expected.push_back({4, 0, 0});
+  expected.push_back({3, 1, 0});
+  expected.push_back({3, 0, 1});
+  expected.push_back({2, 2, 0});
+  expected.push_back({2, 1, 1});
+  expected.push_back({2, 0, 2});
+  expected.push_back({1, 3, 0});
+  expected.push_back({1, 2, 1});
+  expected.push_back({1, 1, 2});
+  expected.push_back({1, 0, 3});
+  expected.push_back({0, 4, 0});
+  expected.push_back({0, 3, 1});
+  expected.push_back({0, 2, 2});
+  expected.push_back({0, 1, 3});
+  expected.push_back({0, 0, 4});
+
+  REQUIRE(expected.size() == results.size());
+  REQUIRE(expected == results);
+
+  // (4,0,0)
+  // (3, 1, 0)
+  // (3, 0, 1)
+  // (2, 2, 0)
+  // (2, 1, 1)
+  // (2, 0, 2);
+  // (1, 3, 0);
+  // (1, 2, 1);
+  // (1, 1, 2);
+  // (1, 0, 3);
+  // (0, 4 , 0);
+  // (0, 3, 1);
+  // (0, 2, 2);
+  // (0, 1, 3);
+  // (0, 0, 4);
 }
