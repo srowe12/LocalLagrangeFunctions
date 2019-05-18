a54a48d76782d2b67e43404383b75f9f6b894ed8
Stephen Rowe <srowe12@gmail.com>
2019-05-18 15:30:06 -0600

    Remove print statement
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- src/lib/math_utils/test/polynomial_test.cpp
+++ src/lib/math_utils/test/polynomial_test.cpp
@@ -2,16 +2,8 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 #include <math_utils/polynomials.h>
-#include <stdio.h>
 
-template <size_t N> void print(const Tuples<N> &t) {
-  for (auto &tuple : t) {
-    for (auto &i : tuple) {
-      std::cout << i << ",";
-    }
-    std::cout << "\n";
-  }
-}
+
 TEST_CASE("BuildTuples") {
   const auto results = buildTuples<3, 4>();
 
