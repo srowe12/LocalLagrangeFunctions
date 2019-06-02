c1233f82ee2d49ac1c099a7fb6debe6c0b831250
Stephen Rowe <srowe12@gmail.com>
2019-06-01 18:26:47 -0600

    Delcare omp simd on RBF operator()'s
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- include/rbf/thin_plate_spline.h
+++ include/rbf/thin_plate_spline.h
@@ -2,10 +2,12 @@
 #define LOCAL_LAGRANGE_THIN_PLATE_SPLINE_H
 
 #include <armadillo>
+#include <omp.h>
 
 template <size_t Dimenion = 2, size_t Degree = 1>
 struct ThinPlateSpline {
   // r represents distance between points squared
+#pragma omp declare simd
   inline double operator()(const double dist_squared) const {
     return .5 * dist_squared * std::log(dist_squared);
   }
